bc147eb032f2bf78a5aa0dfbdf30ce1ba1f8cb17
Jaime Paiva <p2p.digitalbusiness@gmail.com>
2025-01-06 22:47:31 -0300

    feature: adicionado main

---
+++ main.tf
@@ -0,0 +1,22 @@
+provider "null" {}
+
+resource "null_resource" "create_directory_and_file" {
+  connection {
+    type        = "ssh"
+    host        = "servidores-linux.70-71-bc-12-b3-87@cloud.shellhub.io"
+    user        = "jaime"
+    private_key = var.ssh_private_key  # Usando a chave privada do GitHub Secrets
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "mkdir -p /home/jaime/novo_diretorio_terraform",  # Criar o diretório
+      "echo 'Este é o conteúdo do arquivo' > /home/jaime/novo_diretorio_terraform/arquivo.txt"  # Criar o arquivo
+    ]
+  }
+}
+
+# Variáveis
+variable "ssh_private_key" {
+  type = string
+}
